9ca6f354f4a8e25168709df1d5197d154f754430
Mohan -Pasalapudi <venkateshpms123@gmail.com>
2024-05-14 16:27:27 +0530

    redering list items in react,array of objects ,sorting, localecompare

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,7 +1,24 @@
 function List(){
-    const fruits =["orange","sapota","apple","papaya","mango","guava","banana"]
-    fruits.sort();
-    const listItems = fruits.map(fruit =>  <li>{fruit}</li>);
+    const fruits =[{id:1, name:"orange", cal:95},
+                    {id:1, name:"sapota",cal:45},
+                    {id:1, name:"apple",cal:105},
+                    {id:1, name:"papaya",cal:159},
+                    {id:1, name:"mango",cal:37},
+                    {id:1, name:"guava",cal:34},
+                    {id:1, name:"banana",cal:28}];
+    //fruits.sort((a,b) => a.name.localeCompare(b.name)); // alphabetical order
+    //fruits.sort((a,b)=> b.name.localeCompare(a.name)); //reverse alphabetical order
+       //fruits.sort((a,b) => a.cal - b.cal); //numerical sorting 
+       //fruits.sort((a,b) => b.cal - a.cal);  // reverse numerical
+       
+
+       //const lowCal =fruits.filter(fruit => fruit.cal < 100)
+       const highCal =  fruits.filter(fruit => fruit.cal >= 100);
+    
+    const  listItems = highCal.map(fruit => <li key={fruit.id}>
+                                             {fruit.name}:&nbsp;
+                                             <b>{fruit.cal}</b>
+                                            </li>);
     return(<ul>{listItems}</ul>);
 }
 
